3a16fc92ddc77cbd0e2588227c7bd2e278100c3f
devrodriguesz <dev.emersonrodrigues@outlook.com>
2022-09-19 18:17:33 -0300

    Inicializando app com firebase e styled components instalados.

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import firebase from './src/services/firebaseConnection';
+
+
+export default function App() {
+ return (
+   <View>
+    <Text>Olá mundo!</Text>
+   </View>
+  );
+}
