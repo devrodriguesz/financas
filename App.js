65d1182fac36126d0bad695bf79a458d7bd8174d
devrodriguesz <dev.emersonrodrigues@outlook.com>
2022-09-19 22:15:55 -0300

    Estilizando pagina SignIn e removendo seu Header

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
+import {NavigationContainer} from '@react-navigation/native'
 import { View, StatusBar } from 'react-native';
 import firebase from './src/services/firebaseConnection';
 
