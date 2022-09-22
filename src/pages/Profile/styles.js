52df8b7652c34a4197fb3b5127206317c72c1a62
devrodriguesz <dev.emersonrodrigues@outlook.com>
2022-09-22 18:06:33 -0300

    Adicionando as paginas New e Profile, atualizando as rotas do app, estilizando e criando as funcionalidades para a pagina Profile.

---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.View`
+flex: 1;
+background-color: #131313;
+align-items: center;
+`
+
+export const Nome = styled.Text`
+color: #fff;
+text-align: center;
+font-size: 28px;
+margin-top: 25px;
+margin-bottom: 25px;
+`
+
+export const NewLink = styled.TouchableOpacity`
+align-items: center;
+justify-content: center;
+background-color: #00b94a;
+width: 90%;
+height: 45px;
+border-radius: 10px;
+margin-bottom: 10px;
+`
+
+export const NewText = styled.Text`
+color: #fff;
+font-size: 18px;
+font-weight: bold;
+`
+
+export const Logout = styled.TouchableOpacity`
+align-items: center;
+justify-content: center;
+background-color: #c62c36;
+width: 90%;
+height: 45px;
+border-radius: 10px;
+`
+
+export const LogoutText = styled.Text`
+color: #fff;
+font-size: 18px;
+font-weight: bold;
+`
+
